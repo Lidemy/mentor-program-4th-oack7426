92beeea4aa39bd01ecc36ff3031c32121757478f
Annie <annie_yang@addcn.com>
2020-06-29 10:44:25 +0800

    先補上題目敘述

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,3 +1,13 @@
+// 給定一字串，把第一個字轉成大寫之後「回傳」，若第一個字不是英文字母則忽略。
+
+// capitalize('nick')
+// 正確回傳值：Nick
+
+// capitalize('Nick')
+// 正確回傳值：Nick
+
+// capitalize(',hello')
+// 正確回傳值：,hello
 function capitalize(str) {
   
 }
