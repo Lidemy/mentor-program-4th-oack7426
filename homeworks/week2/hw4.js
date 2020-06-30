4634be05da6932254daae81c829a5ffd8b98436a
Annie <annie_yang@addcn.com>
2020-06-30 15:02:38 +0800

    hw4 100%

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,20 +1,12 @@
-// TODO: 未完成
 // 先幫大家複習一下數學，給定一個數字 n，因數就是所有小於等於 n 又可以被 n 整除的數，所以最明顯的例子就是 1 跟 n，這兩個數一定是 n 的因數。現在請寫出一個函式來「印出」所有的因數
-
-// printFactor(10)
-
-// 正確輸出：
-// 1
-// 2
-// 5
-// 10
-// printFactor(7)
-
-// 正確輸出：
-// 1
-// 7
 function printFactor(n) {
-  
+var arrayNum =[];
+  for(var i =0 ;i<=n ;i++){
+      if (n%i ==0){
+        arrayNum.push(i);
+      }
+  }
+  console.log(arrayNum.join('\n'));
 }
 
 printFactor(10);
