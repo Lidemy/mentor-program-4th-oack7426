7918380b51554f2eb28d550a6d9b18b48e41933d
Annie <annie_yang@addcn.com>
2020-07-01 16:13:05 +0800

    week2作業修改

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,7 +1,7 @@
 // 給定一字串，把第一個字轉成大寫之後「回傳」，若第一個字不是英文字母則忽略。
 function capitalize(str) {
   var array =[];
-  var pattern = new RegExp("[A-Za-z]+");
+  var pattern = new RegExp("[A-Za-z]");
   array = str.split('');
   if(pattern.test(str[0])){
     array[0] = array[0].toUpperCase();
