8031151979a7369823bf262dc017ad1a56d9a1b4
oack7426 <oack7426@gmail.com>
2020-07-10 00:34:16 +0800

    hw3 100%

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-// TODO:卡關先coommit回去再想
 var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin
@@ -14,20 +13,23 @@ rl.on('line', function (line) {
 rl.on('close', function () {
   solve(lines)
 })
+/* eslint-disable */
 
-function solve(lines) {
-  for (let i = 0; i < lines.length; i += 1) {
+function solve(num) {
+  for (let i = 1; i < num.length; i += 1) {
     let factor = [];
-    for (let ii = 2; ii < lines[i]; ii += 1) {
-      if (lines[i] % ii === 0) {
+    for (let ii = 0; ii < num[i]; ii += 1) {
+      if (num[i] % ii === 0) {
         factor.push(ii);
       }
     }
-    if (factor.length == 0) {
-      console.log('Prime');
-    } else {
+    if (factor.length >= 2) {
       console.log('Composite');
+    } else if (factor.length == 0) {
+      console.log('Composite');
+    } else {
+      console.log('Prime');
     }
   }
 }
-/* eslint-enable */
+
